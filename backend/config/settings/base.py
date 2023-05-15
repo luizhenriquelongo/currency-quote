518eedf5c0d8a8b7a2925c33518d893e05e46c12
Luiz Longo <longo.luizh@gmail.com>
2023-05-15 20:40:28 -0300

    fix: remove unecessary django stuff like admin

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -29,23 +29,14 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # Application definition
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
     "django.contrib.staticfiles",
     "currencies",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
