ce05b229f0837277ea490fa866f05e81613c06a9
Luiz Longo <longo.luizh@gmail.com>
2023-05-15 20:39:48 -0300

    fix: update urls to use view from currencies

--- backend/currencies/views.py
+++ backend/currencies/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from currencies.vat_comply_api import VATComplyAPI
+
+
+def rates(request):
+    api = VATComplyAPI()
+    return JsonResponse(api.get_rates().dict())
