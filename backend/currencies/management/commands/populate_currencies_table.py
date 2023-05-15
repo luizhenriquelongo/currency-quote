d853e69d88c2688c8b75032a1760c168de0808d7
Luiz Longo <longo.luizh@gmail.com>
2023-05-15 20:43:11 -0300

    build: create a command to populate currencies table with api data

---
+++ backend/currencies/management/commands/populate_currencies_table.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from currencies.models import Currency
+from currencies.vat_comply_api import VATComplyAPI
+
+
+class Command(BaseCommand):
+    help = "Fetches currencies from VATComply API and adds them to the database if not present"
+
+    def handle(self, *args, **options):
+        api = VATComplyAPI()
+        response = api.get_currencies()
+
+        if response is None:
+            self.stdout.write(self.style.ERROR("Couldn't fetch currencies, please try again latter."))
+
+        with transaction.atomic():
+            for currency in response:
+                Currency.objects.update_or_create(code=currency.code, defaults=currency.dict())
+
+        self.stdout.write(self.style.SUCCESS("Currencies fetched and added successfully."))
