bcf5ef11769b30fa8462d496c36c0c897f4a5e72
Luiz Longo <longo.luizh@gmail.com>
2023-05-22 01:15:33 -0300

    fix: change database configuration

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,8 +9,4 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"), engine="django.db.backends.postgresql_psycopg2"
-    )
-}
+DATABASES = {"default": dj_database_url.config()}
