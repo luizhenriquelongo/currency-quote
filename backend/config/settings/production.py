dde3c246f3ce8d4ec3c5ebe2ed5095fd2a686be4
Luiz Longo <longo.luizh@gmail.com>
2023-05-22 01:11:00 -0300

    fix: add engine

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,4 +9,8 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"), engine="django.db.backends.postgresql_psycopg2"
+    )
+}
