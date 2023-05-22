a1863d799e2066792671a036661dcff250e9ee2a
Luiz Longo <longo.luizh@gmail.com>
2023-05-21 23:31:21 -0300

    feat: add dj_database_url to connect to db instance on production

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,17 +1,18 @@
+import dj_database_url
+
 from config.settings.base import *
 
-# TODO: Change this file
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=1800,
+        conn_health_checks=True,
+    )
 }
